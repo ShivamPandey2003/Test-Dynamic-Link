cc14180f5f5f9c19b40e8a057f5c6e40f266b29c
Shivam Pandey <shivam.pandey@knowledgeexcel.com>
2025-08-14 18:06:36 +0530

    updated code

--- src/controllers/Upload.controller.ts
+++ src/controllers/Upload.controller.ts
@@ -1,55 +1,55 @@
 import { NextFunction, Request, Response } from "express";
 import { fileUpload } from "../models/file.model";
 
-export const UploadFiles = async (
-  req: Request,
-  res: Response,
-  next: NextFunction
-) => {
-  try {
-    if (!req.file)
-      return res.status(404).json({
-        message: "File not found",
-      });
+// export const UploadFiles = async (
+//   req: Request,
+//   res: Response,
+//   next: NextFunction
+// ) => {
+//   try {
+//     if (!req.file)
+//       return res.status(404).json({
+//         message: "File not found",
+//       });
 
-    const { filename, destination, mimetype } = req.file;
+//     const { filename, destination, mimetype } = req.file;
 
-    const fileFullPath = destination + filename;
+//     const fileFullPath = destination + filename;
 
-    let uploadFile = await fileUpload.findOne({ fileFullPath });
+//     let uploadFile = await fileUpload.findOne({ fileFullPath });
 
-    if (uploadFile) {
-      return res.status(400).json({ message: "file already exist" });
-    }
+//     if (uploadFile) {
+//       return res.status(400).json({ message: "file already exist" });
+//     }
 
-    uploadFile = new fileUpload({
-      filename: filename,
-      mimeType: mimetype,
-      path: fileFullPath,
-    });
-    await uploadFile.save();
-    return res.json({ status: 200, success: "file uploaded successfully" });
-  } catch (err: any) {
-    return next(
-      res.status(500).json({
-        error: err.message,
-      })
-    );
-  }
-};
+//     uploadFile = new fileUpload({
+//       filename: filename,
+//       mimeType: mimetype,
+//       path: fileFullPath,
+//     });
+//     await uploadFile.save();
+//     return res.json({ status: 200, success: "file uploaded successfully" });
+//   } catch (err: any) {
+//     return next(
+//       res.status(500).json({
+//         error: err.message,
+//       })
+//     );
+//   }
+// };
 
 export const DynamicLick = async (req: Request, res: Response) => {
-  const { url } = req.query;
+  const { url, gear, ID } = req.query;
   if (!url) {
     return res.status(404).send("Link not found");
   }
   const userAgent = req.headers["user-agent"] || "";
 
+  const encodedUrl = encodeURIComponent(`${url}&gear=${gear}&ID=${ID}`);
+
   const storeUrl = /android/i.test(userAgent)
     ? "https://play.google.com/store/apps/details?id=com.myapp"
-    : "https://apps.apple.com/app/id123456789";
-
-  const encodedUrl = encodeURIComponent(`${url}`);
+    : `${url}&gear=${gear}&ID=${ID}`;
 
   const deepLink = `surveypoc://Survey?url=${encodedUrl}`
 
